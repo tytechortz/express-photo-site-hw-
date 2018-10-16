f76dd483b1044e6a2a5f6660c18302832920e204
James <tytechortz@gmail.com>
2018-10-15 18:55:30 -0600

    .gitignore

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+//require('./db/db');
+
+
+
+
+
 
 
 app.listen(3000, () => {
